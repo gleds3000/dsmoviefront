6c450a00b3b7050887f6c134ac7571deb9e8ab90
Gleds3000 <gleds3000@gmail.com>
2022-06-03 00:20:46 -0300

    netlify commit url base

--- frontend/src/utils/requests.ts
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = "https://dsmovie-gleds3000.herokuapp.com" 
-//process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8081";
-//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8081";
-//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
