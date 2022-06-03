9f3251f36c4ede09b6b5f1cf516e86517f2214e3
Gleds3000 <gleds3000@gmail.com>
2022-06-02 22:40:51 -0300

    netlify commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import Form from 'pages/Form';
+import Listing from 'pages/Listing';
+import Navbar from "components/Navbar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/utils/requests.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = "https://dsmovie-gleds3000.herokuapp.com" 
+//process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8081";
+//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8081";
+//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
